3bb9d75d48bb9a03854f68ea298cdc0cbbc14752
yuryi.ustinovich <ustinhtc@gmail.com>
2022-04-19 20:04:23 +0300

    add working with cache to controller

--- www/Controllers/HomeController.cs
+++ www/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Logging;
 using www.Models;
 
@@ -12,14 +13,28 @@ namespace www.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly IDistributedCache _cache;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger, IDistributedCache cache)
 		{
 			_logger = logger;
+			_cache = cache;
 		}
 
 		public IActionResult Index()
 		{
+			var key = "aspnet:{yuryi}:bio";
+			var bio = _cache.GetString(key);
+
+			if (string.IsNullOrEmpty(bio))
+			{
+				bio = "of 32 years old"; // here we imitate call of slow and thick service
+
+				_cache.SetString(key, bio);
+			}
+
+			ViewData["AdditionalMessage"] = $"Yuryi, {bio}!";
+
 			return View();
 		}
 
